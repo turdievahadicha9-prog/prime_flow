da9daeb6d8eed37d1359f8d9d867cd60b8695132
Hadicha <turdievahadicha9@gmail.com>
2026-09-06 20:29:22 +0500

    Update urls

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from about.views import about_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', about_page, name='home'),
     path('api/', include('about.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
